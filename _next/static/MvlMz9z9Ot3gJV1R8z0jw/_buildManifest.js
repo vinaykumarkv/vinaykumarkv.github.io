f1ad8bf0cfcd43d58fadaeef802c648b4694bea2
Vinay Kumar K V <kvvk.win@gmail.com>
2026-07-06 10:06:08 +0100

    Updates

---
+++ _next/static/MvlMz9z9Ot3gJV1R8z0jw/_buildManifest.js
@@ -0,0 +1,18 @@
+self.__BUILD_MANIFEST = {
+  "/": [
+    "static/chunks/0r0yhu7m1pzf9.js"
+  ],
+  "/_error": [
+    "static/chunks/1x-k46elsl4hs.js"
+  ],
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/",
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
---
+++ _next/static/chunks/3zker5wr_i3sd.js
@@ -0,0 +1,6 @@
+__turbopack_load_page_chunks__("/_app", [
+  "static/chunks/0y9vtj6d65fmv.js",
+  "static/chunks/3_c-iswkbtcf9.js",
+  "static/chunks/1u_2dkx3xgzdz.css",
+  "static/chunks/turbopack-1dqnepkmw0mmz.js"
+])
